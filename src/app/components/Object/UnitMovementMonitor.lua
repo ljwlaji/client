31f49d0befaf363bad4156ce940fd96fc0815480
ljw <602809934@qq.com>
2019-10-30 20:01:47 +0800

    [逻辑] 优化Movment检测

--- src/app/components/Object/UnitMovementMonitor.lua
+++ src/app/components/Object/UnitMovementMonitor.lua
@@ -70,41 +70,23 @@ function UnitMovementMonitor:updateDirection()
 	self:getOwner():getPawn():setFlippedX(self.m_Direction == "left" and true or false)
 end
 
-function UnitMovementMonitor:updateHorizonOffset(diff, isJumpping)
+function UnitMovementMonitor:updateMovement(diff, isJumpping)
 	local offset = self:getOwner():isControlByPlayer() and self:onControllerMove(diff, isJumpping) or self:onAIMove(diff, isJumpping)
-	if math.abs(offset.x) == 0 then return false end
-	local finalPos = self:getOwner():getMap():tryFixPosition( self:getOwner(), offset )
-	self:getOwner():move(finalPos)
-	self:updateDirection()
-	return true
-end
-
-function UnitMovementMonitor:updateVerticalOffset(diff)
-	local offset = { x = 0, y = 0 }
 	local currState = self.m_StateMachine:getCurrentState()
 	if currState == MovementStates.STATE_JUMP_HIGH then
-		self.m_FallSpeed = self.m_FallSpeed * 0.9
 		if self.m_FallSpeed < 1 then self.m_StateMachine:setState(MovementStates.STATE_JUMP_FALL) end
+		self.m_FallSpeed = self.m_FallSpeed * 0.9
 	else
-		local TotalYOffset = 0
-		local calcTime = math.abs(math.modf(self.m_FallSpeed / MAX_FALL_SPEED))
-		local extraFallOffset = math.fmod(self.m_FallSpeed, MAX_FALL_SPEED )
-		offset.y = -MAX_FALL_SPEED
-		while calcTime > 1 do
-			local finalPos, hitGround = self:getOwner():getMap():tryFixPosition( self:getOwner(), offset )
-			self:getOwner():move(finalPos)
-			if hitGround then return true end
-			calcTime = calcTime - 1
-			return hitGround
-		end
-		offset.y = extraFallOffset
-		local finalPos, hitGround = self:getOwner():getMap():tryFixPosition( self:getOwner(), offset )
 		self.m_FallSpeed = self.m_FallSpeed * 1.1
 	end
 	offset.y = self.m_FallSpeed
 	local finalPos, hitGround = self:getOwner():getMap():tryFixPosition( self:getOwner(), offset )
-	if hitGround then self.m_FallSpeed = -1 end
+	if hitGround then 
+        self.m_StateMachine:setState(math.abs(offset.x) == 0 and MovementStates.STATE_IDLE or MovementStates.STATE_RUN)
+		self.m_FallSpeed = -1 
+	end
 	self:getOwner():move(finalPos)
+	self:updateDirection()
 	return hitGround
 end
 
@@ -132,10 +114,7 @@ function UnitMovementMonitor:onEnterIdle()
 end
 
 function UnitMovementMonitor:onExecuteIdle(diff)
-	local nextState = nil
-	if self:updateHorizonOffset(diff) then nextState = MovementStates.STATE_RUN end
-	if not self:updateVerticalOffset(diff) then nextState = MovementStates.STATE_JUMP_FALL end
-	if nextState then self.m_StateMachine:setState(nextState) self.m_JumpDirection = self.m_Direction end
+	self:updateMovement()
 end
 
 function UnitMovementMonitor:onExitIdle()
@@ -148,8 +127,7 @@ function UnitMovementMonitor:onEnterIdleRun()
 end
 
 function UnitMovementMonitor:onExecuteIdleRun(diff)
-	self:updateHorizonOffset(diff)
-	if not self:updateVerticalOffset(diff) then self.m_StateMachine:setState(MovementStates.STATE_JUMP_FALL) end
+	self:updateMovement(diff)
 end
 
 function UnitMovementMonitor:onExitIdleRun()
@@ -161,10 +139,7 @@ function UnitMovementMonitor:onEnterRun()
 end
 
 function UnitMovementMonitor:onExecuteRun(diff)
-	local nextState = nil
-	if not self:updateHorizonOffset(diff) then nextState = MovementStates.STATE_IDLE end
-	if not self:updateVerticalOffset(diff) then nextState = MovementStates.STATE_JUMP_FALL end
-	if nextState then self.m_StateMachine:setState(nextState) self.m_JumpDirection = self.m_Direction end
+	self:updateMovement(diff)
 end
 
 function UnitMovementMonitor:onExitRun()
@@ -179,8 +154,7 @@ function UnitMovementMonitor:onEnterJumpHigh()
 end
 
 function UnitMovementMonitor:onExecuteJumpHigh(diff)
-	self:updateHorizonOffset(diff, true)
-	self:updateVerticalOffset(diff)
+	self:updateMovement(diff, true)
 end
 
 function UnitMovementMonitor:onExitJumpHigh()
@@ -192,11 +166,7 @@ function UnitMovementMonitor:onEnterJumpFall()
 end
 
 function UnitMovementMonitor:onExecuteJumpFall(diff)
-	self:updateHorizonOffset(diff, true)
-	if self:updateVerticalOffset(diff) then
-		local offset = self:getOwner():isControlByPlayer() and self:onControllerMove(diff) or self:onAIMove(diff)
-		self.m_StateMachine:setState(math.abs(offset.x) == 0 and MovementStates.STATE_IDLE or MovementStates.STATE_RUN)
-	end
+	self:updateMovement(diff, true)
 end
 
 function UnitMovementMonitor:onExitJumpFall()
