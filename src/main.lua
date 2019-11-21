9d7c16b44ad7dcf68efdf7ac9b32609e5a283a0c
ljw <602809934@qq.com>
2019-11-22 00:54:02 +0800

    Bugly Committing...

--- src/main.lua
+++ src/main.lua
@@ -3,11 +3,11 @@ cc.FileUtils:getInstance():setPopupNotify(false)
 require "config"
 require "cocos.init"
 
-
 --Director::restart()
 
 local function main()
     require("app.MyApp"):create():run()
+	__G__TRACKBACK__("This is a test message for tencent bugly....")
 end
 
 local status, msg = xpcall(main, __G__TRACKBACK__)
