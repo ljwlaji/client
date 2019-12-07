27564915b952f12168c3c23083cca846759eb378
ljw <602809934@qq.com>
2019-12-07 21:16:51 +0800

    [补充提交]

--- src/app/views/ViewBaseEx.lua
+++ src/app/views/ViewBaseEx.lua
@@ -14,27 +14,10 @@ function ViewBaseEx:autoAlgin()
 	if self.m_Children["node_Right_Up"] 		then self.m_Children["node_Right_Up"]:move(display.width, display.height):setAnchorPoint(1, 1) end
 	if self.m_Children["node_Right"] 			then self.m_Children["node_Right"]:move(display.width, display.cy):setAnchorPoint(1, 0.5) end
 	if self.m_Children["node_Right_Buttom"] 	then self.m_Children["node_Right_Buttom"]:move(display.width, 0):setAnchorPoint(1, 0) end
-end
 
-function ViewBaseEx:onEnter_()
-	if self.onEnter then self:onEnter() end
-end
-
-function ViewBaseEx:onExit_()
-	if self.onExit then self:onExit() end
-end
-
-function ViewBaseEx:onEnterTransitionFinish_()
-	if self.onEnterTransitionFinish then self:onEnterTransitionFinish() end
-end
-
-function ViewBaseEx:onExitTransitionStart_()
-	if self.onExitTransitionStart then self:onExitTransitionStart() end
-end
-
-function ViewBaseEx:onCleanup_()
-	if self._isNodeSyncUpdateEnabled == true then display.getWorld():removeNodeFromSyncUpdateList(self) end
-	if self.onCleanup then self:onCleanup() end
+	self:onNodeEvent("cleanup", function()
+		if self._isNodeSyncUpdateEnabled == true then display.getWorld():removeNodeFromSyncUpdateList(self) end
+	end)
 end
 
 function ViewBaseEx:sendAppMsg(msgID, ...)
