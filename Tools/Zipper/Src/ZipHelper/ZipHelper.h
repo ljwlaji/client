bbf1b5bb39d970009473db0646665ddb9b4b5cea
ljw <602809934@qq.com>
2021-07-09 13:06:11 +0800

    [工具库] 去除无用的cpp/h文件

--- Tools/Zipper/Src/ZipHelper/ZipHelper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace ZipHelper
-{
-	static bool Compress(const char* InBuffer, std::string& OutBuffer);
-	static bool UnCompress(const char* InBuffer, std::string& OutBuffer);
-}
--- Tools/Zipper/Src/ZipHelper/main.cpp
+++ Tools/Zipper/Src/ZipHelper/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "ZipHelper.h"
 #include "Zipper.h"
 
 using namespace std;
